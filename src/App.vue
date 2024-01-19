896237a90677f86c54dc2724d7a512a00599dd8b
Alexnander Nasokin <alexnas2python@gmail.com>
2024-01-19 13:23:01 +0300

    Initial install, setup and clean

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header class="flex">
+    <div>
+      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="35" height="35" />
+    </div>
+
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
