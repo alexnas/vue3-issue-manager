56befc973e9c0043eb4e28a43e832721d90941db
Alexnander Nasokin <alexnas2python@gmail.com>
2024-01-27 13:26:55 +0300

    Make DarkModeButton and use VueUse to switch dark-light mode.

--- src/components/header/AppHeader.vue
+++ src/components/header/AppHeader.vue
@@ -1,14 +1,18 @@
 <script setup lang="ts">
 import NavBar from '@/components/header/NavBar.vue'
+import DarkModeButton from '@/components/shared/DarkModeButton.vue'
 </script>
 
 <template>
   <header class="flex items-center justify-between bg-gray-800 p-4">
     <img alt="Vue logo" src="@/assets/logo.svg" width="30" height="30" />
 
-    <nav class="flex gap-8 p-0">
-      <NavBar />
-    </nav>
+    <div class="flex">
+      <nav class="flex gap-6">
+        <NavBar />
+      </nav>
+      <div class="-mt-1 ml-6"><DarkModeButton /></div>
+    </div>
   </header>
 </template>
 
---
+++ src/components/shared/DarkModeButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useDark, useToggle } from '@vueuse/core'
+import { Icon } from '@iconify/vue'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+</script>
+
+<template>
+  <button
+    class="cursor-pointer rounded-lg border-2 border-gray-700 p-2 text-center text-xl font-medium text-teal-700 hover:bg-gray-600 hover:text-orange-400 dark:border-gray-500 dark:text-teal-500 dark:hover:bg-gray-600 dark:hover:text-orange-400"
+    @click="toggleDark()"
+  >
+    <Icon v-if="isDark" class="w-5" :icon="'heroicons:sun-20-solid'" :inline="true" />
+    <Icon v-else class="w-5" :icon="'solar:moon-bold'" :inline="true" />
+  </button>
+</template>
+
+<style scoped></style>
