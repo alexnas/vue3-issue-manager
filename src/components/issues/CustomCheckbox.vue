61ee39bdbab8e5b23183d96be7a0b7e6baa29e20
Alexnander Nasokin <alexnas2python@gmail.com>
2024-02-22 20:09:34 +0300

    Make issueForm (modal) with initial content and validation (vee-validate & yup - install and setup).

---
+++ src/components/issues/CustomCheckbox.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+const props = defineProps({
+  modelValue: { type: [Array, Boolean] },
+  value: { type: [Boolean, Object] },
+  label: { type: String },
+  isDisabled: { type: Boolean }
+})
+const emit = defineEmits(['update:modelValue'])
+const model = computed({
+  get() {
+    return props.modelValue
+  },
+  set(value) {
+    emit('update:modelValue', value)
+  }
+})
+</script>
+
+<template>
+  <div class="my-2 flex items-center">
+    <input
+      type="checkbox"
+      v-model="model"
+      :value="value"
+      :disabled="isDisabled"
+      class="mr-4 h-5 w-5"
+    />
+    <span>{{ label }}</span>
+  </div>
+</template>
--- src/components/modal/BaseModal.vue
+++ src/components/modal/BaseModal.vue
@@ -19,20 +19,24 @@ const { isModalActive } = storeToRefs(modalStore)
   <Teleport to="body">
     <div
       v-show="isModalActive"
-      class="absolute left-0 top-0 flex h-screen w-full justify-center bg-black bg-opacity-30 px-8"
+      class="absolute left-0 top-0 flex h-screen w-full justify-center bg-black bg-opacity-60 px-8"
     >
       <div
         v-if="isModalActive"
-        class="relative mx-auto mt-32 w-11/12 max-w-screen-md self-start bg-gray-100 p-6 text-green-700 shadow-md sm:rounded-lg md:w-2/3"
+        class="relative mx-auto mt-32 w-11/12 max-w-screen-md self-start bg-gray-100 p-6 text-teal-700 shadow-md dark:bg-gray-700 dark:text-teal-300 sm:rounded-lg md:w-2/3"
       >
         <h1 class="mb-4 text-2xl">{{ modalTitle }}</h1>
         <button
-          class="absolute right-0 top-0 mr-5 mt-4 cursor-pointer rounded text-gray-400 transition duration-150 ease-in-out hover:text-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-600"
+          class="absolute right-0 top-0 mr-5 mt-4 cursor-pointer rounded text-gray-400 transition duration-150 ease-in-out hover:text-orange-600 focus:outline-none focus:ring-2 focus:ring-gray-600 dark:focus:ring-gray-500"
           @click="$emit('close-modal')"
           aria-label="close modal"
           role="button"
         >
-          <Icon class="h-5 w-5" :icon="'ant-design:close-outlined'" :inline="true" />
+          <Icon
+            class="h-5 w-5 dark:bg-gray-700 dark:text-gray-300 dark:hover:text-orange-500"
+            :icon="'ant-design:close-outlined'"
+            :inline="true"
+          />
         </button>
 
         <slot />
