5cec2aa853d8762b483c65e4ba7aeef014ea75da
Alexnander Nasokin <alexnas2python@gmail.com>
2024-03-03 21:42:52 +0300

    Justify view of KanbanSwitcher button in the appHeader.

--- src/kanban/KanbanSwitcher.vue
+++ src/kanban/KanbanSwitcher.vue
@@ -1,15 +1,28 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia'
 import { useUserSettings } from '@/stores/userSettings'
+import { activeNavMenuItem, inactiveNavMenuItem } from '@/assets/styles/twClasses'
 
 const userSettingsStore = useUserSettings()
 const { isKanbanView } = storeToRefs(userSettingsStore)
 </script>
 
 <template>
-  <div class="flex gap-2 text-white">
-    <button @click="isKanbanView = true">Kanban</button>
-    <button @click="isKanbanView = false">Table</button>
+  <div class="flex gap-1 text-white">
+    <button
+      class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700"
+      :class="[isKanbanView ? activeNavMenuItem : inactiveNavMenuItem]"
+      @click="isKanbanView = true"
+    >
+      Kanban
+    </button>
+    <button
+      class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700"
+      :class="[!isKanbanView ? activeNavMenuItem : inactiveNavMenuItem]"
+      @click="isKanbanView = false"
+    >
+      Table
+    </button>
   </div>
 </template>
 
